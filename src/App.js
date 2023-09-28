1bd3c07bb6b12e021fff1ce9256e4111c823238b
eyoel demis <eyoeldemis3.gmail.com>
2023-09-28 11:13:47 -0700

    full code commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+import HomePage from "./page/HomePage";
+import AdminPage from "./page/adminPage";
+import About from "./page/About";
+import Contact from "./page/Contact";
+// import Products from "./page/Products";
+import AdminLogin from "./page/AdminLogin";
+import AdminRegister from "./page/AdminRegister";
+import Create from "./page/Create";
+import View from "./page/View";
+import Logout from "./page/Logout";
+import ProductDetails from "./components/ProductDetails";
+import Favorites from "./page/Favorites";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <Navbar /> */}
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/admin" element={<AdminPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/adminLogin" element={<AdminLogin />} />
+        <Route path="/adminRegister" element={<AdminRegister />} />
+        <Route path="/create" element={<Create />} />
+        <Route path="/view" element={<View />} />
+        <Route path="/logout" element={<Logout />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/page/Product.jsx
@@ -0,0 +1,123 @@
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { BsHandThumbsUp } from "react-icons/bs";
+import { BsHandThumbsDown } from "react-icons/bs";
+import axios from "axios";
+
+const Product = ({ data }) => {
+  const { _id: id, name, price, description, files } = data;
+  const [favorites, setFavorites] = useState([]);
+  const navigate = useNavigate();
+  const [file, setFile] = useState([]);
+
+  useEffect(() => {
+    const fetchFavorites = async () => {
+      const response = await axios.get("http://localhost:3001/admin/favorites");
+
+      // console.log("response", response.data.data);
+      setFavorites(response.data.data);
+    };
+    fetchFavorites();
+  });
+
+  const handleClick = () => {
+    console.log("data " + data);
+    // console.log(id, name, price);
+    navigate(`/product/${id}`);
+  };
+
+  const checkFavorites = (id) => {
+    const boolean = favorites.some((fav) => fav._id === id);
+    return boolean;
+  };
+
+  const handleFavorites = async (data) => {
+    console.log("thumbs up");
+    // console.log(data);
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/admin/favorites",
+
+        {
+          data,
+          // files: files,
+        }
+      );
+
+      console.log("response", response.data);
+
+      setFile(response.data.data.files);
+
+      console.log("favorite", response.data.data);
+
+      if (response.status === 200) {
+        if (files) {
+          try {
+            console.log("file", files);
+
+            const favoriteId = response.data.data._id;
+
+            console.log("favoriteId" + favoriteId);
+          } catch (error) {
+            console.error(error);
+          }
+        }
+      }
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+
+  const removeFavorites = async (id) => {
+    console.log("ThumbsDown");
+
+    try {
+      const response = await axios.delete(
+        `http://localhost:3001/admin/favorites/${id}`
+      );
+
+      console.log(response.data.data);
+      setFavorites(response.data.data);
+    } catch (ex) {
+      console.log(ex);
+    }
+  };
+
+  return (
+    <div className="product" key={id}>
+      {/* <img src={productImage} /> */}
+      <div className="description">
+        {data.files.map((file) => {
+          // console.log(file.path);
+          return (
+            <img
+              src={`http://localhost:3001/admin/${file.path}`}
+              onClick={handleClick}
+            />
+          );
+        })}
+        
+        <div className="product-info">
+          <div className="product-description">
+            <div>
+              <p className="product-p">{name}</p>
+              <p>{price} birr</p>
+            </div>
+            {checkFavorites(id) ? (
+              <button onClick={() => removeFavorites(id)}>
+                <BsHandThumbsDown className="thumbs-up-icon" />
+              </button>
+            ) : (
+              <button onClick={() => handleFavorites(data)}>
+                <BsHandThumbsUp className="thumbs-up-icon" />
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Product;
